e82ec7dcbee2a4f1026493cc1348f850e71b657d
A19MariaGM <113421900+A19MariaGM@users.noreply.github.com>
2022-09-27 04:53:13 +0200

    commit con ejercicio 3_10

--- garcia_miras_maria_del_pilar_tarefa_3_10.php
+++ garcia_miras_maria_del_pilar_tarefa_3_10.php
@@ -28,6 +28,64 @@
 
 <body>
 
+    <?php
+    $username = $password = $cityEmployment = $servers = $role = $sign = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (empty($_POST["username"])) {
+            $nameErr = "Username is required";
+        } else {
+            $username =
+                test_input($_POST["username"]);
+        }
+
+        if (empty($_POST["password"])) {
+            $nameErr = "Password is required";
+        } else {
+            $password =
+                test_input($_POST["password"]);
+        }
+
+        if (empty($_POST["cityEmployment"])) {
+            $nameErr = "cityEmployment is required";
+        } else {
+            $cityEmployment =
+                test_input($_POST["cityEmployment"]);
+        }
+
+        if (empty($_POST["servers"])) {
+            $nameErr = "Server is required";
+        } else {
+            $servers =
+                test_input($_POST["servers"]);
+        }
+
+        if (empty($_POST["role"])) {
+            $nameErr = "Role is required";
+        } else {
+            $role =
+                test_input($_POST["role"]);
+        }
+
+        if (empty($_POST["sign"])) {
+            $nameErr = "sign is required";
+        } else {
+            $sign =
+                test_input($_POST["sign"]);
+        }
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
+    ?>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <table>
             <tr>
@@ -40,7 +98,7 @@
             </tr>
             <tr>
                 <td class="col1"><label for="cityEmployment">City of Employment:</label></td>
-                <td class="col2"><input type="text" id="cityEmployement" name="cityEmployement"></td>
+                <td class="col2"><input type="text" id="cityEmployment" name="cityEmployment"></td>
             </tr>
             <tr>
                 <td class="col1"><label for="servers">Web server:</label></td>
@@ -58,10 +116,10 @@
                 <td class="col1">Please specify your role:</td>
                 <td class="col2">
                     <ul style="list-style-type: none; padding:0px;">
-                        <li><input type="radio" name="admin" value="admin">Admin</li>
-                        <li><input type="radio" name="engineer" value="engineer">Engineer</li>
-                        <li><input type="radio" name="manager" value="manager">Manager</li>
-                        <li><input type="radio" name="guest" value="guest">Guest</li>
+                        <li><input type="radio" name="role" value="admin">Admin</li>
+                        <li><input type="radio" name="role" value="engineer">Engineer</li>
+                        <li><input type="radio" name="role" value="manager">Manager</li>
+                        <li><input type="radio" name="role" value="guest">Guest</li>
                     </ul>
                 </td>
             </tr>
@@ -69,13 +127,13 @@
                 <td class="col1">Single Sign-on <br> to the following::</td>
                 <td class="col2">
                     <ol style="list-style-type: none; padding:0px;">
-                        <li><input type="checkbox" id="sign1" name="sign1" value="Mail">
+                        <li><input type="checkbox" id="sign1" name="sign" value="Mail">
                             <label for="sign1">Mail</label><br>
                         </li>
-                        <li><input type="checkbox" id="sign2" name="sign2" value="Payroll">
+                        <li><input type="checkbox" id="sign2" name="sign" value="Payroll">
                             <label for="sign2">Payroll</label><br>
                         </li>
-                        <li><input type="checkbox" id="sign3" name="sign3" value="Self-service">
+                        <li><input type="checkbox" id="sign3" name="sign" value="Self-service">
                             <label for="sign3">Self-service</label><br>
                         </li>
                     </ol>
@@ -87,13 +145,15 @@
                 </td>
                 <td style="text-align:left">
                     <input type="submit" value="Login">
-                    <input type="submit" value="Reset">
+                    <input type="reset" value="Reset">
                 </td>
             </tr>
 
         </table>
     </form>
-
+    <?php
+    echo $username . ", " . $password . ", " . $cityEmployment . ", " . $servers . ", " . $role . ", " . $sign;
+    ?>
 </body>
 
 </html>
